49ccd1923dd49737dd2e23ac9c2e9234d3a66bda
Daniel Szymanek <danekszy@users.noreply.github.com>
2017-04-23 10:59:41 +0200

    Fix annoying Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+        "indent": ["error", 4]
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    }
 };
