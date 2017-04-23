c7d85ad70cf7cdcad6b38b1f0d9fae969826f1e1
Daniel Szymanek <danekszy@users.noreply.github.com>
2017-04-23 13:09:55 +0200

    Refactor code, add external config, implement login server feedback

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 const CLIENT_URL = 'http://localhost:3000';
 const readline = require('readline');
+const EOL = require('os').EOL;
+const util = require('util');
 const socketClient = require('socket.io-client');
-let socket;
+
+let connection;
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,30 +15,51 @@ const userData = {
     name: null,
 };
 
-const askForAMsg = () => {
-    rl.question(`${userData.name}: \n`, (body) => {
-        socket.emit('message', { userData, body });
+const writeLine = (line, ...args) => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(util.format(line, ...args) + EOL);
+};
 
+const askForAMsg = () => {
+    rl.prompt();
+};
+
+const initChatLogging = () => {
+    connection.on('broadcast', (data) => {
+        writeLine(`🗯  ${data.userName}: ${data.message}`);
         askForAMsg();
+    });
+
+    rl.on('line', (line) => {
+        connection.emit('message', { message: line });
+
+        rl.prompt();
     });
 };
 
 const initConnection = () => {
-    socket = socketClient(CLIENT_URL);
-    socket.on('broadcast', (data) => {
-        console.log(`🗯 ${data.name}: ${data.body}`);
-    });
+    connection = socketClient(CLIENT_URL);
+    connection.emit('login', { userData });
 
-    askForAMsg();
+    connection.on('loginResult', (result) => {
+        if (result.success) {
+            console.log(`👋  Hello ${userData.name}!`);
+            rl.setPrompt('> ');
+            initChatLogging();
+            askForAMsg();
+        } else {
+            console.log(`Login failed. Error: ${result.errorMsg}`);
+            initApp();
+        }
+    });
 };
 
 const initApp = () => {
     rl.question('Whats your name? \n', (name) => {
         userData.name = name;
-        console.log(`Hello ${name}! 👋`);
         initConnection();
     });
 };
 
 initApp();
-//   rl.close();
--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 #!/usr/bin/env node
-const io = require('socket.io')(3000);
+const config = require('./config/server.config');
+const io = require('socket.io')(config.socketPort);
+const ChatServer = require('./lib/ChatServer');
 
-io.on('connection', (socket) => {
-    socket.emit('message', { body: 'hello world' });
-    console.log('Somebody connected');
-    
-    socket.on('message', (data) => {
-        console.log(`Client: ${JSON.stringify(data)}`);
-        const safeData = {
-            name: data.userData.name,
-            body: data.body
-        }
-        io.sockets.emit('broadcast', safeData);
-    });
-});
+const server = new ChatServer({ io });
